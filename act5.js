29d2cd231a9d300e807198a8282bb352164a7b37
lara2309 <tololala05@gmail.com>
2024-10-04 08:50:18 -0300

    act5.js

---
+++ act5.js
@@ -0,0 +1,6 @@
+const prompt = require('prompt-sync')();
+
+var celsius, fare
+fare = prompt("Ingresa grados Fahrenheit: ");
+var celsius= (fare- 32) * (5/9);
+console.log ("la temperatura celsius es: ", celsius)
